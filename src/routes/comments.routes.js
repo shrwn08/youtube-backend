6cc7d67cd149c061faacd5700fb3e78adf32485d
Shrawan <shrwnkumar08@gmail.com>
2025-11-13 23:40:14 +0530

    fix bug like update comments to create comments

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { updateComment , getComments} from '../controllers/comments.controllers.js';
+import { createComment, getComments } from '../controllers/comments.controllers.js';
 import verifyToken from '../utils/verification.utils.js';
 
 const commentRoutes = express.Router();
 
-
-
-commentRoutes.post('/:videoId/comments', verifyToken ,updateComment);
-commentRoutes.get('/:videoId/comments', getComments);
-
+commentRoutes.post('/videos/:videoId', verifyToken, createComment);
+commentRoutes.get('/videos/:videoId', getComments);
 
 export default commentRoutes;
